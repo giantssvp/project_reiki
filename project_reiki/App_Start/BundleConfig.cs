a60b42d7f3e33974390227a48e8e5b6918a43354
Vitthal Padwal <vitthalpadwal89@gmail.com>
2017-08-31 18:49:58 +0530

    Removed garbage

--- project_reiki/App_Start/BundleConfig.cs
+++ project_reiki/App_Start/BundleConfig.cs
@@ -14,18 +14,12 @@ namespace project_reiki
                         "~/Scripts/jquery.history_remote.pack.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/jquery.tabs.css",
                       "~/Content/templatemo_style.css"));
